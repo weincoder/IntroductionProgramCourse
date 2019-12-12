8f3a3e36a9cd572300a0e4562b2bacdc5c4ad141
daniherr <daniherr@bancolombia.com.co>
2019-12-12 12:37:50 -0500

    subiendo primera clase

---
+++ Session1_Hello_World_Using_Variables/hello_world.py
@@ -0,0 +1,40 @@
+#En python la función print permite mostrar en pantalla un mensaje, su contenido debe ser un texto
+print("hello world")
+
+#Hablando de texto las variables de este tipo son denotadas como strings
+frase="hello world"
+print(frase)
+
+#Con la función type podemos evaluar de que tipo de variables estamos tratando
+print(type(frase))
+
+#En este caso nos retorna que es un String
+
+#Las variables del tipo entero son del tipo int
+print(type(6))
+
+numero=4
+
+print(type(numero))
+
+#las variables númericas con decimales son del tipo float
+
+print(type(4.3))
+
+dinero=0.2
+
+print(type(dinero))
+#tambien podemos mostrar en pantalla mensajes incluyendo textos y variables separados por coma
+print("usted esta pobre tiene",dinero, "en pesos colombianos")
+
+#Existe otro tipo de variable booleana solo vale falso o verdadero
+
+prueba_embarazo=True
+
+print(type(prueba_embarazo))
+
+print("The result is ...", prueba_embarazo)
+
+
+
+
