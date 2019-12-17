835cb1ed75ca341242bb33945fec9c8097a62c61
daniherr <daniherr@bancolombia.com.co>
2019-12-17 14:01:49 -0500

    subiendo instrucciones utiles con strings

--- Session8_StringsUtils/str_utils.py
+++ Session8_StringsUtils/str_utils.py
@@ -22,7 +22,42 @@ print ("la palabra más larga de la lista es {}".format(
 #palabras por lo tanto su len sera igual a 10
 print (len ("hola mundo") == 10)
 
+#Si deseamos poner en mayuscula la primera letra de la frase
+txt = "hola a todos"
+txt_m = txt.capitalize()
+print (txt_m)
 
+#Si deseamos convertir a minuscula la cadena completa
+txt = "NO ESTOY EN MINUSCULA"
+txt_m= txt.casefold()
+print(txt_m)
+#Si deseamos ubicar en medio de un espacio de n caracteres
+#un string en la mitad
+txt = "Centro"
+print(txt.center(20))
 
+#contar las ocurrencias es decir la cantidad de veces que aparece
+txt = "jamas diría las mimas cosas jamas jamas!!"
+print(txt.count("jamas"))
 
+#verificar que termine in a specific caracter
+txt = "debo terminar en punto ."
+print (txt.endswith("."))
 
+#encontrar donde esta una palabra
+txt = "donde estara la palabra en medio de tantas palabras?"
+print(txt.find("palabra"),txt[16:23])
+#nos indica que inicia en el espacio 16 y como palabra tiene 7 
+#espacios imprimimos el espacio 16 al 23
+
+#comprobar que sea númerico el contenido del string
+txt = "345678"
+print (txt.isnumeric())
+
+#reemplazar de forma eficiente en una cadena las palabras
+txt = "me gusta programar en java"
+print (txt.replace ("java","python"))
+
+#Convertir a mayuscula todas las letras de un string
+txt = "estoy claramente en minuscula"
+print(txt.upper())
